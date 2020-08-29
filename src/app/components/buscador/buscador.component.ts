208d97e34ad839033dfcb1c357c0162ea9328668
Seba1705 <Sebaaguirre2012@gmail.com>
2020-08-28 23:30:37 -0300

    Separando codigo

---
+++ src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+    selector: 'app-buscador',
+    templateUrl: './buscador.component.html'
+})
+export class BuscadorComponent implements OnInit {
+    
+    heroes: Heroe[] = [];
+    termino: string;
+
+    constructor( private _heroesService:HeroesService, private activatedRoute : ActivatedRoute) { }
+  
+    ngOnInit() {
+        this.activatedRoute.params.subscribe( params => {
+            this.termino = params['termino'];
+            this.heroes = this._heroesService.buscarHeroes( params['termino'] );
+        });
+    }
+  
+}
